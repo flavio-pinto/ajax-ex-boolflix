535d68573a3c45f188336c973d831af2b5df8321
Flavio Pinto <flaviopnt@hotmail.it>
2020-05-05 12:31:11 +0200

    Milestone 1 - Added optimizations with functions + validations

--- js/main.js
+++ js/main.js
@@ -12,35 +12,61 @@ $(document).ready(function () {
     var source = $('#results-template').html();
     var template = Handlebars.compile(source);
 
+    // Search movie
     searchButton.click(function(){
-        $.ajax({
-            url: 'https://api.themoviedb.org/3/search/movie',
-            method: 'GET',
-            data: {
-                api_key: 'a4ea792285866fc38f076bcbb9cdca74',
-                language: 'it-IT',
-                query: searchBar.val()
-            },
-            success: function(res){
-                resultsArea.html('');
-                var searchResults = res.results;
-                for(var i = 0; i < searchResults.length; i++){
-                    var singleResult = searchResults[i];
-                    var context = {
-                        title: singleResult.title,
-                        originalLanguage: singleResult.original_language,
-                        originalTitle: singleResult.original_title,
-                        rating: singleResult.vote_average
-                    };
-                    var html = template(context);
-                    resultsArea.append(html);
+        var query = searchBar.val().trim();
+
+        if(query !== ''){
+            $.ajax({
+                url: 'https://api.themoviedb.org/3/search/movie',
+                method: 'GET',
+                data: {
+                    api_key: 'a4ea792285866fc38f076bcbb9cdca74',
+                    language: 'it-IT',
+                    query: query
+                },
+                success: function(res){
+                    var searchResults = res.results;
+
+                    if(searchResults.length > 0) {
+                        printMovies(template, searchResults, resultsArea);
+                    } else {
+                        alert('Nessun film trovato');
+                        searchBar.select();
+                    }
+                },
+                error: function(){
+                    alert('Errore chiamata API');
                 }
-            },
-            error: function(){
-                console.log('ERRORE');
-            }
-        });
+            });
+        } else {
+            alert('Hai inserito un campo vuoto! Per favore, inserisci un valore per la ricerca');
+            searchBar.focus();
+        }
 
         searchBar.val('');
     });
 }); // <---- end document ready
+
+// Funzione per stampare i risultati
+function printMovies(template, movies, container){
+    // Reset lista
+    resetContainer(container);
+    // Loop sugli elementi della lista
+    for(var i = 0; i < movies.length; i++){
+        var movie = movies[i];
+        var context = {
+            title: movie.title,
+            originalLanguage: movie.original_language,
+            originalTitle: movie.original_title,
+            rating: movie.vote_average
+        };
+        var html = template(context);
+        container.append(html);
+    }
+}
+
+// Funzione di reset
+function resetContainer(element){
+    element.html('');
+}
