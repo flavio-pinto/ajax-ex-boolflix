f5b989e1fcd98ced79fb08d67cf838aaf80c48ce
Flavio Pinto <flaviopnt@hotmail.it>
2020-05-06 18:24:43 +0200

    Milestone 4 - Completed
    I will do some refactoring

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ function printShows(type, template, shows){
             originalTitle: originalTitle,
             rating: printStarsRating(show.vote_average),
             type: type,
-            overview: show.overview
+            overview: show.overview.substr(0, 550) + '...'
         };
         var html = template(context);
         resultsArea.append(html);
